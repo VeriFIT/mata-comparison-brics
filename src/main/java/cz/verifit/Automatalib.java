0744204af0e3b15cabd1a665f5e6a8eec4805209
verifit <verifit@fit.vutbr.cz>
2023-09-18 14:09:24 +0200

    Fix return value for error

--- src/main/java/cz/verifit/Automatalib.java
+++ src/main/java/cz/verifit/Automatalib.java
@@ -13,13 +13,14 @@ public class Automatalib {
             pathsToAutomata = new ArrayList<>(Arrays.asList(args).subList(1, args.length));
         } else {
             System.err.println("error: Program expects at least one argument: path to .emp file and paths to automata");
-            return;
+            System.exit(1);
         }
 
         try {
             (new EmpParser()).parseAndInterpret(input, pathsToAutomata, true);
         } catch (Exception ex) {
             System.err.println("error: " + ex.getMessage());
+            System.exit(1);
         }
     }
 }
