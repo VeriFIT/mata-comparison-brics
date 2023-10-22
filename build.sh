8bca9b19d71a6361f73d6e28fccfdd6d1d8dde3e
verifit <verifit@fit.vutbr.cz>
2023-10-22 14:14:47 +0200

    Add test for online

--- build.sh
+++ build.sh
@@ -3,6 +3,14 @@
 BASEDIR=$(realpath $(dirname "$0"))
 PROJECTDIR=$(realpath "$BASEDIR/../..")
 
+wget -q --spider http://google.com
+if [ $? -ne 0 ]; then
+  echo -e "\e[1;33mwarning: you are offline"
+  echo -e "In order to build with maven, please, turn your network on."
+  echo -e "Or use our prebuilt jar archive in '../../bin' directory.\e[0m"
+  exit 0
+fi
+
 mvn clean package
 if [ $? -ne 0 ]; then
   exit 1
